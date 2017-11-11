d732b527c2f45cf41ec915ffe5918bb934cae1b8
MidnightSeer <midnightseer@thevanoutside.com>
2017-11-10 20:57:18 -0600

    Update port-scan.py

--- hercules/scan/port-scan.py
+++ hercules/scan/port-scan.py
@@ -2,7 +2,7 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import optparse
+import optparse,sys
 from socket import *
 from threading import *
 
@@ -10,17 +10,19 @@ screenLock = Semaphore(value=1)
 
 
 def connScan(tgtHost, tgtPort):
+    global debug
     try:
         connSkt = socket(AF_INET, SOCK_STREAM)
         connSkt.connect((tgtHost, tgtPort))
-        connSkt.send('ViolentPython\r\n')
-        results = connSkt.recv(100)
+        connSkt.send('\r\n'.encode())
+        results = connSkt.recv(1024).decode()
         screenLock.acquire()
         print('[+] %d/tcp open' % tgtPort)
         print('[+] ' + str(results))
     except:
         screenLock.acquire()
-        print('[-] %d/tcp closed' % tgtPort)
+        if debug:
+            print('[-] %d/tcp closed' % tgtPort)
     finally:
         screenLock.release()
         connSkt.close()
@@ -35,9 +37,9 @@ def portScan(tgtHost, tgtPorts):
 
     try:
         tgtName = gethostbyaddr(tgtIP)
-        print('\n[+] Scan Results for: ' + tgtName[0])
+        print('[+] Scan Results for: ' + tgtName[0])
     except:
-        print('\n[+] Scan Results for: ' + tgtIP)
+        print('[+] Scan Results for: ' + tgtIP)
 
     setdefaulttimeout(1)
     for tgtPort in tgtPorts:
@@ -46,21 +48,29 @@ def portScan(tgtHost, tgtPorts):
 
 
 def main():
+    global debug
     parser = optparse.OptionParser('usage %prog ' + \
                                    '-H <target host> -p <target port>')
-    parser.add_option('-H', dest='tgtHost', type='string', \
+    parser.add_option('-H', dest='tgtHost', type='string',
                       help='specify target host')
-    parser.add_option('-p', dest='tgtPort', type='string', \
+    parser.add_option('-d', action='store_true',dest='debug',default=False,
+                      help='enable debug mode')
+    parser.add_option('-p', dest='tgtPort', type='string',
                       help='specify target port[s] separated by comma')
 
     (options, args) = parser.parse_args()
-
+    tgtPorts = options.tgtPort
     tgtHost = options.tgtHost
-    tgtPorts = str(options.tgtPort).split(',')
+    debug = options.debug
 
     if (tgtHost == None) | (tgtPorts[0] == None):
         print(parser.usage)
         exit(0)
+
+    if "," in options.tgtPort:
+        tgtPorts = str(options.tgtPort).split(',')
+    elif "-" in options.tgtPort:
+        tgtPorts = list(range(1,int(options.tgtPort.split('-')[1])))+[len(list(range(1,int(options.tgtPort.split('-')[1]))))+1]
 
     portScan(tgtHost, tgtPorts)
 
