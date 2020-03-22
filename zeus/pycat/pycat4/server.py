f28f72f3279322a39522081e4b85bc5b841d0573
midnightseer <21272239+MidnightSeer@users.noreply.github.com>
2020-03-22 15:10:29 -0400

    fix upload

--- zeus/pycat/pycat4/server.py
+++ zeus/pycat/pycat4/server.py
@@ -133,7 +133,7 @@ def file_transfer_get(conn, command):      #get file from server
 
 def file_transfer_put(conn, commands):       #push file to server
     send_data(conn, commands + "[END]")
-    file_name = path.basename(commands.split()[0])
+    file_name = commands.split()[1]
     if VERBOSE:
         print_info("Trying to open: {}".format(file_name))
     f = open(file_name, 'rb')
